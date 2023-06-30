0ccd232df64bd061c077f377c01f7c64c56b9cac
Maxime MARCHAND <mmarchand6@myges.fr>
2023-06-30 14:27:39 +0200

    Merge conflicts

--- src/domains/auth/auth.guard.ts
+++ src/domains/auth/auth.guard.ts
@@ -3,7 +3,7 @@ import { AuthGuard, IAuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class JwTAuthGuard extends AuthGuard('jwt') implements IAuthGuard {
+export class JwtAuthGuard extends AuthGuard('jwt') implements IAuthGuard {
   public canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
--- src/domains/users/users.controller.ts
+++ src/domains/users/users.controller.ts
@@ -1,81 +1,65 @@
 import {
-  Body,
-  ClassSerializerInterceptor,
   Controller,
-  Delete,
-  Get,
   Inject,
-  Param,
-  Patch,
   UseGuards,
   UseInterceptors,
   HttpCode,
   ParseUUIDPipe,
 } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { RoleAdminGuard } from '../auth/admin.guard';
-import { JwTAuthGuard } from '../auth/auth.guard';
+import { JwtAuthGuard } from '../auth/auth.guard';
 import { ChangePasswordDto } from './dto/update-password.dto';
 import { UpdateUserRoleDto } from './dto/update-role.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './users.entity';
 import { UsersService } from './users.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ClassSerializerInterceptor } from '@nestjs/common';
 
 @ApiTags('Users')
 @ApiBearerAuth()
-@Controller('users')
+
+@Controller()
 export class UsersController {
   @Inject(UsersService)
   private readonly userService: UsersService;
 
-  @Get()
-  @HttpCode(200)
+  @MessagePattern('getAllUsers')
+  @UseGuards(JwtAuthGuard, RoleAdminGuard)
   @UseInterceptors(ClassSerializerInterceptor)
-  @UseGuards(JwTAuthGuard, RoleAdminGuard)
   public getAllUsers(): Promise<User[]> {
     return this.userService.getAllUsers();
   }
 
-  @Get('/:id')
-  @HttpCode(200)
-  @UseGuards(JwTAuthGuard)
+  @MessagePattern('getOneUser')
+  @UseGuards(JwtAuthGuard)
   @UseInterceptors(ClassSerializerInterceptor)
-  public getOneUser(
-    @Param('id', new ParseUUIDPipe()) id: string,
-  ): Promise<User> {
+  public getOneUser(@Payload('id') id: string): Promise<User> {
     return this.userService.getOneUser(id);
   }
 
-  @Patch('/password')
-  @HttpCode(200)
-  @UseGuards(JwTAuthGuard)
-  public updatePassword(@Body() body: ChangePasswordDto): Promise<object> {
+  @MessagePattern('updatePassword')
+  @UseGuards(JwtAuthGuard)
+  public updatePassword(@Payload() body: ChangePasswordDto): Promise<object> {
     return this.userService.updatePassword(body);
   }
 
-  @Patch('/:id/role')
-  @HttpCode(200)
-  @UseGuards(JwTAuthGuard, RoleAdminGuard)
-  public updateRole(
-    @Param('id', new ParseUUIDPipe()) id: string,
-    @Body() body: UpdateUserRoleDto,
-  ): Promise<object> {
-    return this.userService.updateRole(id, body.role);
+  @MessagePattern('updateRole')
+  @UseGuards(JwtAuthGuard, RoleAdminGuard)
+  public updateRole(@Payload('id') id: string, @Payload('role') role: string): Promise<object> {
+    return this.userService.updateRole(id, role);
   }
 
-  @Patch()
-  @HttpCode(200)
-  @UseGuards(JwTAuthGuard)
-  public updateProfile(@Body() body: UpdateUserDto): Promise<object> {
+  @MessagePattern('updateProfile')
+  @UseGuards(JwtAuthGuard)
+  public updateProfile(@Payload() body: UpdateUserDto): Promise<object> {
     return this.userService.updateProfile(body);
   }
 
-  @Delete('/:id')
-  @HttpCode(204)
-  @UseGuards(JwTAuthGuard, RoleAdminGuard)
-  public deleteUser(
-    @Param('id', new ParseUUIDPipe()) id: string,
-  ): Promise<object> {
+  @MessagePattern('deleteUser')
+  @UseGuards(JwtAuthGuard, RoleAdminGuard)
+  public deleteUser(@Payload('id') id: string): Promise<object> {
     return this.userService.deleteUser(id);
   }
 }
